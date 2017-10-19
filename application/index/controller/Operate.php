0863298c697337d0f41c4c05c49c046cb7620de5
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-19 18:30:17 +0800

    update

---
+++ application/index/controller/Operate.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+class Operate extends Controller{
+    public function index(){
+        return $this->fetch('admin/operate/index');
+    }
+    public function moments(){
+        return $this->fetch('admin/operate/moments');
+    }
+    public function xianshi(){
+//        $data=array('data'=>'chenwenzheng');
+//        $ch = curl_init ();
+//        curl_setopt ($ch, CURLOPT_URL, 'http://192.168.11.131:8383/test.php');
+//        curl_setopt ($ch, CURLOPT_POST, true );
+//        curl_setopt ($ch, CURLOPT_HEADER, 0 );
+//        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1 );
+//        curl_setopt ($ch, CURLOPT_POSTFIELDS, $data );
+//        $state=curl_exec ($ch );
+//        curl_close ($ch );
+//        return $state;
+
+        $data = input('post.content');
+        echo $data;
+    }
+    public function group(){
+        return $this->fetch('admin/group/index');
+    }
+    //测试方法
+    public function test(){
+        $data = input('post.data');
+        echo json_encode($data);
+    }
+}
