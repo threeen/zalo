2e9e18f68bbf48a39d41d216ce3f7d2e9c825f22
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-23 14:51:31 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -142,7 +142,7 @@ class Phone extends Controller{
         $data = input('post.account');
             if(isset($_COOKIE['admin'])) {
                 $data = trim($data);
-                $searchData = model('Accounts')->where('username','like','%'.$data.'%')->where(['status'=>1])->select();
+                $searchData = model('Accounts')->where('username','like','%'.$data.'%')->where(['status'=>1])->paginate();
                 return $this->fetch('admin/search',[
                     'searchData'=>$searchData,
                 ]);
