c84d8a0e9d8d6ca8e06899113a49a27710438933
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-24 17:10:12 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -162,7 +162,7 @@ class Phone extends Controller{
     public function test(){
         $data = file_get_contents('public/zalo.txt');
         $arr = explode(' ',$data);
-        $arr = explode(' ',$arr[0]);
+        $arr = explode(' ',$arr);
         print_r($arr);exit;
         foreach($arr as $key=>$value){
             $new = explode('|',$value);
