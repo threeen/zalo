22366eaa74d655d2e8be1d0435c1543c8eee5d10
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-24 13:46:55 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -10,7 +10,7 @@ class Operate extends Base{
             echo "<br>";
             echo strtotime($data[0]['create_time'])+60;
             //exit();
-            if(time()>(strtotime($data[0]['create_time'])+60)){
+            if(time()>(strtotime($data['create_time'])+60)){
                model('Accounts')->save(['login_status'=>0,'create_time'=>date(time())],['id'=>852]);
             }
         }
