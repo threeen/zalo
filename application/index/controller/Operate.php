c2a050c01a19720137f2cbaf1107e34fadeddd2b
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-25 13:34:36 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -60,12 +60,14 @@ class Operate extends Base{
             $str = '';
             foreach($data as $key=>$value){
                 foreach($value as $key=>$val){
-                $username = $val['username'];
-                $password = $val['password'];
-                $latitude = $val['latitude'];
-                $longitude = $val['longitude'];
-                $device_num = $val['device_num'];
-                $str += $username."#".$password."#".$latitude."#".$longitude."#".$device_num."@@";
+                    foreach($val as $key=>$v) {
+                        $username = $v['username'];
+                        $password = $v['password'];
+                        $latitude = $v['latitude'];
+                        $longitude = $v['longitude'];
+                        $device_num = $v['device_num'];
+                        $str += $username . "#" . $password . "#" . $latitude . "#" . $longitude . "#" . $device_num . "@@";
+                    }
                 }
             }
             echo $str;
