f49a20965a2483011193846a0fe935edfe1b3e21
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-25 14:34:24 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -60,7 +60,7 @@ class Operate extends Base{
         if(!empty($data)){
             $str = '';
             for($i=0;$i<count($data);$i++){
-                $str .= $id."-".$data[$i]['username'] . "#" . $data[$i]['password'] . "#" . $data[$i]['latitude'] . "#" . $data[$i]['longitude']. "#" . $data[$i]['device_num'] . "<br />";
+                $str .= $id."-".$data[$i]['username'] . "#" . $data[$i]['password'] . "#" . $data[$i]['latitude'] . "#" . $data[$i]['longitude']. "#" . $data[$i]['device_num'] . "<br>";
             }
             echo $str;
         }else{
