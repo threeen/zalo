b5785feb8222f6bf61bdfded1c87b4ccbfc32f94
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-26 16:41:32 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -94,7 +94,7 @@ $str1=<<<AB
         </body>
         </html>
 AB;
-        $str3 = base64_decode($moments['content']);
+        $str3 = $moments['content'];
         echo $str.$str3.$str1;
     }
 }
