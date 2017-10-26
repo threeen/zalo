41ec6861ce8179661aab242a29231325d96f3625
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-26 11:31:50 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -8,10 +8,10 @@ class Operate extends Controller{
         $id = array();
         if(!empty($data)){
             for($i=0;$i<count($data);$i++){
-                if(time()>(strtotime($data['create_time'])+30)) {
-                    $data['login_status'] = 0;
-                    $data['create_time'] = date('Y-m-d H:i:s', time());
-                    $id[] = $data['id'];
+                if(time()>(strtotime($data[$i]['create_time'])+30)) {
+                    $data[$i]['login_status'] = 0;
+                    $data[$i]['create_time'] = date('Y-m-d H:i:s', time());
+                    $id[$i][] = $data['id'];
                 }
             }
             model('Accounts')->saveAll($data,$id);
