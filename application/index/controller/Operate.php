6170371c0eb2f2a34618dcf9b94d7ac53139afb0
chenwenzheng <chenwenzheng@dayugame.net>
2017-10-26 12:03:26 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -4,15 +4,14 @@ use think\Controller;
 class Operate extends Controller{
     public function index(){
         $data = model('Accounts')->getLiveAccounts();
+        print_r($data[0]['create_time']);exit();
         if(!empty($data)){
-            $id  = array();
-            foreach($data as $key=>$value){
-                if(time()>(strtotime($value['create_time'])+30)){
-                    $id[]['id'] = $value['id'];
-                }
-                model('Accounts')->saveAll(['login_status'=>0,'create_time'=>date('Y-m-d H:i:s',time())],['id'=>$id[$key]['id']]);
-            }
 
+            foreach($data as $value){
+                if(time()>(strtotime($value['create_time'])+60)){
+                    model('Accounts')->save(['login_status'=>0,'create_time'=>date('Y-m-d H:i:s',time())],['id'=>$value['id']]);
+                }
+            }
         }
         return $this->fetch('admin/operate/index',[
             'data' => $data
