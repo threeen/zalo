56f5a7a259816e03d2187b87e40eef6342d4be7e
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-02 16:41:20 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -158,7 +158,7 @@ class Phone extends Controller{
                 $data = input('post.account',$isset,'');
                 cookie('searchData',$data);
                 $data = trim($data);
-                $send_search = input('get.send_search');
+                $send_search = input('send_search');
                 if(isset($send_search)){
                     $searchData = model('NewAccounts')->where('username','like','%'.$data.'%')->where(['status'=>1])->paginate();
                     $page = $searchData->render();
