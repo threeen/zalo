5bc3751d13cdc5a44347bb0a9a5658c91eefbf15
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-03 16:51:47 +0800

    update

--- application/common/model/Accounts.php
+++ application/common/model/Accounts.php
@@ -12,7 +12,7 @@ class Accounts extends Model
     }
     public function getErrorAccountsData(){
         $data = [
-            ['friends','new_friends','nearby']=>['lt',0],
+            [['friends','new_friends','nearby']=>['lt',0]],
             'nearby_per'=>['lt',0],
             'nearby_per'=>['gt',1],
             'status'=>['neq',1]
