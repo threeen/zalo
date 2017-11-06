7bf38bea409e10777097cdbf464887b61e87c69c
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-06 18:03:14 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -196,7 +196,7 @@ class Phone extends Controller{
                     ]);
                 }else{
                     $datas = [
-                        'simulator_num' =>1,
+                        'simulator_num' =>$simulator_num,
                         'friends'=>['egt',0],
                         'new_friends'=>['egt',0],
                         'new_nearby'=>['egt',0],
