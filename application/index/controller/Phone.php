5a8108bf7b51e23de350030fbae7d9770da56c44
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-06 17:42:08 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -203,10 +203,9 @@ class Phone extends Controller{
                         'nearby_per'=>['elt',1],
                         'status'=>['eq',1],
                         'username'=>['like','%'.$data.'%'],
-
+                        'simulator_num' =>['eq',1],
                     ];
-                    $datasss = ['simulator_num' =>1];
-                    $searchData = model('Accounts')->where($datas)->where($datasss)->paginate();
+                    $searchData = model('Accounts')->where($datas)->paginate();
                     $page = $searchData->render();
                     $count = model('Accounts')->getSearchCounts($data);
                     return $this->fetch('admin/search',[
