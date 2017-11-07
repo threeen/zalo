1efdd0b236663dd3bd6084c4ee5b8c0b260a451f
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-07 11:46:00 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -18,7 +18,7 @@ class Phone extends Controller{
             $realCounts = model('Accounts')->getRealCounts();
             $friendsCounts = model('Accounts')->getFriendsAllCounts();
             $dayFriends = model('Accounts')->getDayFriends();
-            $page = $data->render();print_r($page);exit();
+            $page = $data->render();print_r($page['page']);exit();
             return $this->fetch('admin/list',[
                 'data'=>$data,
                 'count'=>$count,
