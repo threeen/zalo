4da277c20b17324c64ef175596e9314727d8bc58
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-07 11:27:18 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -12,7 +12,7 @@ class Phone extends Controller{
     public function list_phone(){
         if(isset($_COOKIE['admin'])) {
             $data = model('Accounts')->getAccountsData();
-            
+            json_encode($data);
             print_r($data);exit();
             $count = model('Accounts')->getCounts();
             $realCounts = model('Accounts')->getRealCounts();
