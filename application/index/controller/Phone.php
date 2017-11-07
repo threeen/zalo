53f89a532e70ac91e21a753fc683961e6dd692eb
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-07 10:27:20 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -24,7 +24,7 @@ class Phone extends Controller{
                 'friendsCounts'=>$friendsCounts,
                 'dayFriends'=>$dayFriends,
                 'page'=>$page,
-                'nowPage'=>$data->getCurrentPage
+                'nowPage'=>$data.page
             ]);
         }
         else{
