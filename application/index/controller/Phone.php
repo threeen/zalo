c63c016e31f37adbfb1375a68f2eddc8569d7d91
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-08 13:49:19 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -275,6 +275,7 @@ class Phone extends Controller{
         //echo $last_simulator_num['simulator_num'];exit();
         $dir="public/zalo帐号/";
         $file=scandir($dir);
+        $file = array_shift(array_shift($file));
         print_r($file);exit;
         $file = fopen("public/zalo帐号", "r");
         $str = $acc = $data = array();
