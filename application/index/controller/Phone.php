50c1425c9118290e1792a631b196359b9e43a088
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-08 14:40:21 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -275,13 +275,9 @@ class Phone extends Controller{
         //echo $last_simulator_num['simulator_num'];exit();
         $dir="public/zalo帐号/";
         $files=scandir($dir);
+        print_r($files);exit;
         for($i=2;$i<count($files);$i++){
-            $start = file_get_contents('public/zalo帐号/'.$files[$i]);
-            $txt = file_get_contents('public/index.txt');
-            $end = file_put_contents('public/index.txt',$txt."<br>".$start);
-        }
-        $txt = file_get_contents('public/index.txt');
-            $file = fopen("public/index.txt", "r");
+            $file = fopen("public/zalo帐号/$files[$i]", "r");
             $str = $acc = $data = array();
             $username = $password = $latitude = $longitude = $device_num = array();
             $i=0;
@@ -325,6 +321,7 @@ class Phone extends Controller{
                 }
                 //$data[$i]['simulator_num']=$last_simulator_num['simulator_num']+1;
             }
+        }
         $result = model('NewAccounts')->allowField(true)->saveAll($data);
         if($result){
             echo "帐号入库成功";
