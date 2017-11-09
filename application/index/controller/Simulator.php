5a683875d3fef2ad2838719de66e260c972ad70c
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-09 15:27:57 +0800

    update

--- application/index/controller/Simulator.php
+++ application/index/controller/Simulator.php
@@ -2,7 +2,5 @@
 namespace app\index\controller;
 use think\Controller;
 class Simulator extends Controller{
-    public function simulator(){
-        return $this->fetch('admin/group/simulator');
-    }
+
 }
