df5aa2efea59a1f423027641d2d65a3c3a6829ab
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-10 09:56:59 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -62,7 +62,7 @@ class Operate extends Controller{
         $start = ($value-1)*80;
         $end = 80;
         $sql = "select new.id,acc.friends,new.username from zl_new_accounts new,zl_accounts acc where
-               new.id>=$start and new.id<$end";
+               new.id>=1 and new.id<80";
         $data = Db::execute($sql);
         return $data;exit();
         $data = model('NewAccounts')->getValueArea($start,$end);
