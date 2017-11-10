ab196f8d77f6aba3a7985b2957050ff33cb50725
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-10 17:03:27 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -79,7 +79,7 @@ class Operate extends Controller{
         $sql_count = "select sum(acc.friends) as friends,sum(acc.new_friends) as new_fri,COUNT(new.username) as valid_acc from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
                 new.id>$start and new.id <= $end and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
                 acc.nearby_per<=1";
-        $data_count = Db::query($sql);
+        $data_count = Db::query($sql_count);
         print_r($data_count);exit;
         $friends = $data_count[0]['friends'];$new_fri = $data_count[0]['new_fri']; $valid_acc = $data_count[0]['valid_acc'];
         $count_all = $data_all."#".$friends."#".$new_fri."#".$valid_acc;
