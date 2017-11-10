d403512a3b49c5d2f1d666824ef2c2d3907a8b6e
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-10 17:58:51 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -56,9 +56,10 @@ class Operate extends Controller{
                 new.id>0 and new.id <= 80 and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
                 acc.nearby_per<=1";
         $data_count = Db::query($sql_count);
+        $time=date('Y-m-d',time());
         $sql_day_count = "select COUNT(new.username) as day_acc from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
                 new.id>0 and new.id <= 80 and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
-                acc.nearby_per<=1 and create_time>date('Y-m-d',time())";
+                acc.nearby_per<=1 and acc.create_time>$time";
         $data_day_count = Db::query($sql_day_count);
         $day_count = $data_day_count[0]['day_acc'];
         $friends = $data_count[0]['friends'];$new_fri = $data_count[0]['new_fri']; $valid_acc = $data_count[0]['valid_acc'];$count_all = $data_all[0]['count'];
