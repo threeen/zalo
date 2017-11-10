057ca033299007383bfa7cc2be895f65c5ecfda9
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-10 17:24:14 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -82,8 +82,8 @@ class Operate extends Controller{
         $data_count = Db::query($sql_count);
         $friends = $data_count[0]['friends'];$new_fri = $data_count[0]['new_fri']; $valid_acc = $data_count[0]['valid_acc'];$count = $data_all[0]['count'];
         $count_all = $count."#".$friends."#".$new_fri."#".$valid_acc;
-        $dataAll['all']=$count_all;
-        echo json_encode($dataAll);
+        $new = array('data'=>$dataAll,'count'=>$count_all);
+        echo json_encode($new);
     }
     //数据回传到模拟器
     public function returnData(){
