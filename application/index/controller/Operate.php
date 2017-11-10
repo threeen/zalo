2608a306437b94e6f0e220e5b6409ba6f2b2c21b
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-10 16:30:15 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -74,7 +74,7 @@ class Operate extends Controller{
         $data_err = Db::query($sql_err);
         //print_r($data_err);exit;
         $dataAll = array_merge($data,$data_err);
-        $sql_sum = "select sum(acc.friends),SUM(acc.new_friends) from zl_accounts acc LEFT JOIN zl_new_accounts new on new.username=acc.username where
+        $sql_sum = "select sum(acc.friends),SUM(acc.new_friends) from zl_accounts acc,zl_new_accounts new where new.username=acc.username and
                 new.id>$start and new.id <= $end and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
                 acc.nearby_per<=1  ORDER BY new.id  ";
         $all_friends = Db::query($sql);
