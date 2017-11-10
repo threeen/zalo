3d6fc806c4a24b0200261e4ea088bfe2a7993333
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-10 09:54:49 +0800

    update

--- application/index/controller/Operate.php
+++ application/index/controller/Operate.php
@@ -64,7 +64,7 @@ class Operate extends Controller{
         $sql = "select new.id,acc.friends,new.username from zl_new_accounts new,zl_accounts acc where
                new.id>=$start and new.id<$end";
         $data = Db::execute($sql);
-        print_r($data);exit();
+        return $data;exit();
         $data = model('NewAccounts')->getValueArea($start,$end);
         $username = $accounts = $new = array();
         foreach($data as $value){
