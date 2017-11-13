14c99de34c1e2b9fadadb7843d991764005e5e47
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-13 15:47:02 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use think\Db;
+
 class Phone extends Controller{
     public function index(){
         if(isset($_COOKIE['admin'])){
@@ -11,7 +13,7 @@ class Phone extends Controller{
     }
     public function list_phone(){
         if(isset($_COOKIE['admin'])) {
-            $data = model('Accounts')->getAccountsData();
+            //$data = model('Accounts')->getAccountsData();
             $count = model('Accounts')->getCounts();
             $realCounts = model('Accounts')->getRealCounts();
             $realDayCounts = model('Accounts')->getDayRealCounts();
@@ -19,6 +21,10 @@ class Phone extends Controller{
             $dayFriends = model('Accounts')->getDayFriends();
             //获取模拟器号
             //$simulator = model('Accounts')->getSimulator();
+            $sql_data = "select acc.id aid,acc.friends,acc.new_friends,acc.new_nearby,acc.nearby_per,acc.status,acc.create_time,acc.username,new.id nid
+                      from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
+                 acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and acc.nearby_per<=1 and acc.status=1 ORDER BY create_time DESC ";
+            $data = Db::query($sql_data);
             $page = $data->render();
             return $this->fetch('admin/list',[
                 'data'=>$data,
