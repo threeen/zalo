36ed42af15532d83c9738f75f58ca79c8512c21f
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-17 14:04:50 +0800

    update

--- application/index/controller/Phone.php
+++ application/index/controller/Phone.php
@@ -98,7 +98,7 @@ class Phone extends Controller{
                     $account = array(
                         'username' => $arr[0],
                         'create_time' => date($arr[3]),
-                       'login_status' => $arr[4],
+                       'login_status' => ($acc['login_status']==1)?0:1,
                     );
                 }else {
                     $account = array(
@@ -108,7 +108,7 @@ class Phone extends Controller{
                         'new_nearby' => $arr[1] - $acc['data2'],
                         'nearby_per' => ($arr[1] - $acc['data2']) / 45,
                         'create_time' => date($arr[3]),
-                        'login_status' => $arr[4],
+                        'login_status' => ($acc['login_status']==1)?0:1,
                         'data1' => $arr[1],
                         'data2' => $arr[2]
                     );
