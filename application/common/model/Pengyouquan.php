93b1668d7e28446bbc4957cb3fe1c4d6c3021804
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-17 16:05:18 +0800

    update

--- application/common/model/Pengyouquan.php
+++ application/common/model/Pengyouquan.php
@@ -6,5 +6,8 @@ use think\Model;
 
 class Pengyouquan extends Model
 {
-
+    //根据帐号名获取当个帐号数据
+    public function getOneAccounts($username){
+        return model('Pengyouquan')->where(['username'=>$username])->find();
+    }
 }
--- application/index/controller/Moments.php
+++ application/index/controller/Moments.php
@@ -17,6 +17,32 @@ class Moments extends Controller
         foreach($data as $value){
             $username .=$value['username']."#";
         }
-        echo $username;
+        return $username;
+    }
+    //对已经发过朋友圈的帐号进行存储记录
+    public function acc_insert(){
+        $data = input('username');
+        if(empty($data)){
+            return "获取不到已发发朋友圈的帐号";
+        }
+
+        $username = $acc = array();
+        $username[] = explode('#',trim($data,"#"));
+
+        for($i=0;$i<count($username);$i++){
+            $exist_user = model('Pengyouquan')->getOneAccounts($username[$i]);
+            if(!$exist_user){
+                echo "该帐号".$exist_user['username']."不存在"."<br>";
+                continue;
+            }else {
+                $acc[$i]['username'] = $username[$i];
+                $acc[$i]['times'] = $exist_user['times']+1;
+                $acc[$i]['create_time'] = date(time());
+            }
+        }
+        $result = model('Pengyouquan')->allowField(true)->saveAll($data);
+        if($result){
+            echo " 发朋友圈帐号入库成功";
+        }
     }
 }
