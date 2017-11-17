a2af8cb117cba2231f700c612ebe1df835e8a53c
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-17 11:32:57 +0800

    update

--- application/index/controller/Simulator.php
+++ application/index/controller/Simulator.php
@@ -8,13 +8,15 @@ class Simulator extends Controller{
         $sql = "select count(*) from zl_new_accounts";
         $count = Db::query($sql);
         $time=strtotime(date('Y-m-d',time()));
-        $data = array(array());
+        $data = array();
+        $j=0;
         for($i=0;$i<$count[0]['count(*)'];$i=$i+80){
         $sql_day_count = "select COUNT(new.username) as day_acc,sum(acc.new_friends) as new_fri from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
                 new.id>$i and new.id <= ($i+80) and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
                 acc.nearby_per<=1 and unix_timestamp(acc.create_time)>$time";
         $data_day_count = Db::query($sql_day_count);
-        $data['new_fri'] = $data_day_count[0]['new_fri']; $data['day_acc'] = $data_day_count[0]['day_acc'];
+        $data[$j]['new_fri'] = $data_day_count[0]['new_fri']; $data[$j]['day_acc'] = $data_day_count[0]['day_acc'];
+            $j++;
         }
         print_r($data);exit;
         return $this->fetch('admin/simulator/simulator');
