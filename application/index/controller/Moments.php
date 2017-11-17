5f3ce9194f885f9f0d571e8d92526afd38718cec
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-17 17:28:04 +0800

    update

--- application/index/controller/Moments.php
+++ application/index/controller/Moments.php
@@ -28,7 +28,7 @@ class Moments extends Controller
 
         $username = $acc = array();
         $username = explode('@',$data);
-
+        print_r($username);exit;
         for($i=0;$i<count($username);$i++){
             $exist_user = model('NewAccounts')->getOneAccounts($username[$i]);
             if(!$exist_user){
