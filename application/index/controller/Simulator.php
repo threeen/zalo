0b79edbf739e91cfc45e978b3ccc5dd5c46faf4e
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-17 11:17:33 +0800

    update

--- application/index/controller/Simulator.php
+++ application/index/controller/Simulator.php
@@ -6,7 +6,7 @@ use think\Db;
 class Simulator extends Controller{
     public function index(){
         $sql = "select count(*) from zl_new_accounts";
-        $count = Db::query($sql);
+        $count = Db::query($sql);echo $count;exit;
         $time=strtotime(date('Y-m-d',time()));
         $data = array();
         for($i=0;$i<$count;$i=$i+80){
