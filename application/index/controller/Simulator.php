76abaeda3aa89754523ac4469fc2e3ea508a29f9
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-20 15:45:46 +0800

    update

--- application/index/controller/Simulator.php
+++ application/index/controller/Simulator.php
@@ -16,6 +16,13 @@ class Simulator extends Controller{
                 acc.nearby_per<=1 and unix_timestamp(acc.create_time)>$time";
         $data_day_count = Db::query($sql_day_count);
         $data[$j]['new_fri'] = $data_day_count[0]['new_fri']; $data[$j]['day_acc'] = $data_day_count[0]['day_acc'];
+
+            $sql = "select acc.create_time as cr_time from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
+                new.id>$i and new.id <= ($i+80) and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
+                acc.nearby_per<=1 and unix_timestamp(acc.create_time)>$time";
+            $data = Db::query($sql);
+            print_r($data);exit;
+
             $j++;
         }
 
