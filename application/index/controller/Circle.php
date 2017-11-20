67fcec95734aa91d88028ba0902e431019a38d7b
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-20 14:57:12 +0800

    update

--- application/index/controller/Circle.php
+++ application/index/controller/Circle.php
@@ -6,10 +6,10 @@ use think\Model;
 class Circle extends Controller
 {
     public function index(){
-        $data = model('Circle')->getData();
-        model('Circle')->getLastSql();exit;
+        //$data = model('Circle')->getData();
+        //model('Circle')->getLastSql();exit;
         return $this->fetch('admin/circle/index',[
-            'data' => $data
+          //  'data' => $data
         ]);
     }
 }
