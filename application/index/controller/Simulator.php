582e6b94f9f91b056199693cbc31cdc0b3dc89b2
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-20 16:00:31 +0800

    update

--- application/index/controller/Simulator.php
+++ application/index/controller/Simulator.php
@@ -15,13 +15,18 @@ class Simulator extends Controller{
                 new.id>$i and new.id <= ($i+80) and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
                 acc.nearby_per<=1 and unix_timestamp(acc.create_time)>$time";
         $data_day_count = Db::query($sql_day_count);
-        $data[$j]['new_fri'] = $data_day_count[0]['new_fri']; $data[$j]['day_acc'] = $data_day_count[0]['day_acc'];
+        $data[$j]['new_fri'] = $data_day_count[0]['new_fri']; $data[$j]['day_acc'] = $data_day_count[0]['day_acc'];$data[$j]['status'] = 1;
 
             $sql = "select acc.create_time as cr_time,acc.login_status from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
                 new.id>$i and new.id <= ($i+80) and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
                 acc.nearby_per<=1 and unix_timestamp(acc.create_time)>$time";
             $data = Db::query($sql);
-            print_r($data);exit;
+            foreach($data as $key => $value){
+                if($data['login_status']==1 && time()>(strtotime($data['cr_time'])+3600)){
+                    $data[$j]['status'] = -1;
+                    break;
+                }
+            }
 
             $j++;
         }
