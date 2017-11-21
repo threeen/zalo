f7ede587da89842048d985dc058389b7b560fac3
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-21 15:40:44 +0800

    update

--- application/index/controller/Simulator.php
+++ application/index/controller/Simulator.php
@@ -18,15 +18,15 @@ class Simulator extends Controller{
         $data_day_count = Db::query($sql_day_count);
         $data[$j]['new_fri'] = $data_day_count[0]['new_fri']; $data[$j]['day_acc'] = $data_day_count[0]['day_acc'];$data[$j]['status'] = 1;
 
-            $sql = "select acc.create_time as cr_time,acc.login_status from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
+            $sql = "select acc.username,acc.create_time as cr_time,acc.login_status from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
                 new.id>$i and new.id <= ($i+80) and acc.friends>=0 and acc.new_friends>=0 and acc.nearby_per>=0 and acc.new_nearby>=0 and
                 acc.nearby_per<=1 and unix_timestamp(acc.create_time)>$time";
             $data_status = Db::query($sql);
             foreach($data_status as $key => $value){
                 if($value['login_status']==1 && time()>(strtotime($value['cr_time'])+3600)){
                     $data[$j]['status'] = -1;
-                    $live_acc [$j]['username']= $value['username'];
-                    $live_acc [$j]['create_time']=$value['create_time'];
+                    $live_acc []['username']= $value['username'];
+                    $live_acc []['create_time']=$value['create_time'];
                 }
             }
             $times = array();
