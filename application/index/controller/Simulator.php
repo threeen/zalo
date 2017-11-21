d2a2b95d3f32139b0136fbb13e6e3524c048c963
chenwenzheng <chenwenzheng@dayugame.net>
2017-11-21 17:12:49 +0800

    update

--- application/index/controller/Simulator.php
+++ application/index/controller/Simulator.php
@@ -33,10 +33,9 @@ class Simulator extends Controller{
                 $sql = "select acc.username from zl_accounts acc,zl_new_accounts new  where new.username=acc.username and
                 new.id>$i and new.id <= ($i+80) and acc.login_status = 1 ORDER BY acc.create_time limit 1";
                 $dd = Db::query($sql);
-                $username  = $dd[0]['username'];
-                $sql_update = "update zl_accounts acc set acc.login_status = 0 join ON zl_new_accounts new  where new.username=acc.username and
-                new.id>$i and new.id <= ($i+80) and acc.login_status = 1 AND acc.username <> $username";
-                Db::execute($sql_update);
+                print_r($dd);
+
+
             }
             $j++;
         }
